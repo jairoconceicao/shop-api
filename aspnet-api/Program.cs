c959dc43d19ed7be7b0fdf681d139a6baab76fd4
Jairo Conceicao <jairoconceicao@gmail.com>
2026-07-05 15:53:22 -0300

    refactor api bootstrap extensions

--- aspnet-api/Program.cs
+++ aspnet-api/Program.cs
@@ -1,15 +1,8 @@
 using System.Text;
+using aspnet_api.Api;
 using aspnet_api.Api.Endpoints;
-using aspnet_api.Api.Middleware;
-using aspnet_api.Api.OpenApi;
-using aspnet_api.Application.Abstractions.Security;
 using aspnet_api.Infrastructure;
-using aspnet_api.Infrastructure.Persistence;
-using aspnet_api.Infrastructure.Security;
 using aspnet_api.src.Application;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,88 +15,11 @@ builder.Host.UseSerilog((context, services, loggerConfiguration) =>
         .Enrich.FromLogContext();
 });
 
-builder.Services.AddOpenApi(options =>
-{
-    options.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
-    options.AddOperationTransformer<BearerSecurityRequirementTransformer>();
-});
-builder.Services.AddApiVersioning(options =>
-{
-    options.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.ReportApiVersions = true;
-    options.ApiVersionReader = new Asp.Versioning.UrlSegmentApiVersionReader();
-}).AddApiExplorer(options =>
-{
-    options.GroupNameFormat = "'v'VVV";
-    options.SubstituteApiVersionInUrl = true;
-});
-
-builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.SectionName));
-var jwtOptions = builder.Configuration.GetSection(JwtOptions.SectionName).Get<JwtOptions>() ?? new JwtOptions();
-
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.RequireHttpsMetadata = !builder.Environment.IsDevelopment();
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = jwtOptions.Issuer,
-            ValidateAudience = true,
-            ValidAudience = jwtOptions.Audience,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Secret)),
-            ClockSkew = TimeSpan.FromSeconds(30)
-        };
-    });
-builder.Services.AddAuthorization();
-
-builder.Services.AddSingleton(TimeProvider.System);
-
-// EF Core DbContext
-var connectionString = builder.Configuration.GetConnectionString("ShopDb")
-    ?? throw new InvalidOperationException("Connection string 'ShopDb' was not found.");
-
-builder.Services.AddDbContext<ShopDbContext>(options => 
-{
-    options.UseNpgsql(connectionString);
-    options.UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
-#if DEBUG
-    options.EnableSensitiveDataLogging();
-    options.EnableDetailedErrors();
-#endif
-});
+builder.Services.AddApiServices(builder.Configuration, builder.Environment);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-
-    app.UseSwaggerUI(options =>
-    {
-        options.RoutePrefix = "swagger";
-        options.SwaggerEndpoint("/openapi/v1.json", "Shop Api v1");
-        options.EnablePersistAuthorization();
-    });
-}
-
-app.UseAuthentication();
-app.UseMiddleware<ValidacaoSessaoJwtMiddleware>();
-app.UseAuthorization();
-
-app.MapEndpoints();
-
-app.UseHttpsRedirection();
-
+app.UseApiPipeline();
 app.Run();
-
-
---
+++ aspnet-api/src/Api/WebApplicationExtensions.cs
@@ -0,0 +1,32 @@
+using aspnet_api.Api.Endpoints;
+using aspnet_api.Api.Middleware;
+
+namespace aspnet_api.Api;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication UseApiPipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.RoutePrefix = "swagger";
+                options.SwaggerEndpoint("/openapi/v1.json", "Shop Api v1");
+                options.EnablePersistAuthorization();
+            });
+        }
+
+        app.UseAuthentication();
+        app.UseMiddleware<ValidacaoSessaoJwtMiddleware>();
+        app.UseAuthorization();
+
+        app.MapEndpoints();
+
+        app.UseHttpsRedirection();
+
+        return app;
+    }
+}
