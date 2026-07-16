e90247d98784573d9d4bf6948693aab3c58476cd
Jairo Conceicao <jairoconceicao@gmail.com>
2026-07-16 11:55:10 -0300

    perf(TASK-125): Reduzir grafo JavaScript inicial

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,12 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { configDefaults, defineConfig } from 'vitest/config'
-import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    topLevelAwait({
-      // Promise export name for each chunk
-      promiseExportName: '__tla',
-      // Promise import name for each chunk
-      promiseImportName: i => `__tla_${i}`})    
-    ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    manifest: true,
+  },
   test: {
     environment: 'jsdom',
     execArgv: ['--no-experimental-webstorage'],
